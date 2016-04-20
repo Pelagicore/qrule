888a485e56fcfd485b68888c715cc4d2c289c3f8
Anders Johansson <anders.johansson@pelagicore.com>
2016-04-20 16:56:08 +0200

    Added o flag for changing output fileName

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,11 @@
 #include <output/xmloutputformatter.h>
 #include <QTextStream>
 
-void printOutput(const QList<KRuleOutput*> &ruleViolations) {
+void printOutput(const QList<KRuleOutput*> &ruleViolations, QString fileName) {
     OutputFormatter* xof = new XMLOutputFormatter(ruleViolations);
     qDebug() << xof->format().toStdString().c_str();
 
-    QFile fl("output.xml");
+    QFile fl(fileName);
     fl.open(QIODevice::WriteOnly);
     fl.write(xof->format().toStdString().c_str());
     fl.close();
@@ -36,9 +36,8 @@ int main(int argv, char *argc[]) {
     QCommandLineOption setDot(QStringList() << "d" << "dotty", QLatin1String("Create dot file for each QML file"));
     commandLine.addOption(setDot);
 
-    QCommandLineOption setOutput(QStringList() << "o" << outPutFile, QLatin1String("Change name of the output file"));
+    QCommandLineOption setOutput(QStringList() << "o" << "outPutFile", QLatin1String("Change name of the output file"),"nameFile", "output.xml");
     commandLine.addOption(setOutput);
-
 
     commandLine.addPositionalArgument(QLatin1String("krule"), QLatin1String("KRule rules file"));
     commandLine.addPositionalArgument(QLatin1String("files"), QLatin1String("list of qml or js files to verify"));
@@ -52,6 +51,8 @@ int main(int argv, char *argc[]) {
     }
 
     bool s_setDot = commandLine.isSet(setDot);
+    bool s_setOutputFile = commandLine.isSet(setOutput);
+    QString name = commandLine.value(setOutput);
 
     QStringList arguments = commandLine.positionalArguments();
     QString kruleFilename = arguments.takeFirst();
@@ -59,7 +60,7 @@ int main(int argv, char *argc[]) {
 
     QList<KRuleOutput*> violations = kruleEngine.verifyQMLFiles(arguments);
 
-    printOutput(violations);
+    printOutput(violations, name);
 
     return violations.isEmpty() ? 0 : 1;
 }
